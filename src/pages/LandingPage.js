adfc86c9be97d68400dff89698b7e0e6179fc774
Gifari Reza Pahlevi <gifari.reza@gmail.com>
2020-05-02 07:16:58 +0700

    add header splice

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import module from 'elements/but'
+import React, { Component } from 'react'
 
-export default function LandingPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Header from 'parts/Header'
+
+export default class LandingPage extends Component {
+    render () {
+        return <>
+            <Header {...this.props}></Header>
+        </>;
+    }
 }
